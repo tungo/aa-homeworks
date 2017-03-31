ec0612c8a6fb2f3a77d13e1b9ddb4bfb38662cc4
Tu Ngo <tungo8810@gmail.com>
2017-03-31 05:59:02 -0700

    FIx assign method in Map class and add test cases

--- w1d4/map.rb
+++ w1d4/map.rb
@@ -5,7 +5,7 @@ class Map
 
   def assign(key, value)
     idx = lookup(key)
-    idx ? @array[idx][key] = value : @array << [key, value]
+    idx ? @array[idx][1] = value : @array << [key, value]
   end
 
   def lookup(key)
@@ -30,5 +30,31 @@ class Map
 
   def show
     self.dup
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  map = Map.new
+  p map.show
+
+  puts "Assign to map"
+  10.times do
+    idx, num = rand(10), rand(10)
+    puts "idx: #{idx}; num: #{num}"
+
+    map.assign(idx, num)
+    p map.show
+  end
+
+  puts "Lookup a key"
+  10.times do
+    key = rand(10)
+    puts "key: #{key} => index: #{map.lookup(key).inspect}"
+  end
+
+  puts "Get value by key"
+  10.times do
+    key = rand(10)
+    puts "key: #{key} => value: #{map.lookup(key).inspect}"
   end
 end
