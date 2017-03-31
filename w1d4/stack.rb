2086ecc7ce0840e52b85dc970a67d21341b4ee49
Tu Ngo <tungo8810@gmail.com>
2017-03-31 04:42:33 -0700

    Fix remove method and add test case

--- w1d4/stack.rb
+++ w1d4/stack.rb
@@ -8,10 +8,21 @@ class Stack
   end
 
   def remove
-    @array.pop(el)
+    @array.pop
   end
 
   def show
     self.dup
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  stack = Stack.new
+  10.times do |num|
+    stack.add(num)
+  end
+  p stack
+
+  10.times { stack.remove }
+  p stack.show
 end
