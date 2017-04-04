e554436d1bd10a7ce9cca9ef1276c0e0ba856e59
Tu Ngo <tungo8810@gmail.com>
2017-04-04 08:39:09 -0700

    Add initialize method

--- w2d1/mancala/lib/board.rb
+++ w2d1/mancala/lib/board.rb
@@ -2,13 +2,25 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+
+    @cups.each_with_index do |cup, idx|
+      if idx != 6 && idx != 13
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    
   end
 
   def make_move(start_pos, current_player_name)
