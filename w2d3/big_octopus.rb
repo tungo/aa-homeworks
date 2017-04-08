55ed234bae21d81f673403128803d20511a781e9
Tu Ngo <tungo8810@gmail.com>
2017-04-08 13:20:20 -0700

    Add #slow_dance and #constant_dance

--- w2d3/big_octopus.rb
+++ w2d3/big_octopus.rb
@@ -68,9 +68,38 @@ end
 p clever(fishes)
 
 def slow_dance(dir, tiles)
-
+  tiles.index(dir)
 end
 
-def constant_dance(dir, tiles)
+tiles_array = [
+  "up",
+  "right-up",
+  "right",
+  "right-down",
+  "down",
+  "left-down",
+  "left",
+  "left-up"
+]
 
+p slow_dance('up', tiles_array)
+p slow_dance('right-down', tiles_array)
+
+
+TILES = {
+  "up" => 0,
+  "right-up" => 1,
+  "right" => 2,
+  "right-down" => 3,
+  "down" => 4,
+  "left-down" => 5,
+  "left" => 6,
+  "left-up" => 7
+}
+
+def constant_dance(dir, new_tiles_data_structure)
+  new_tiles_data_structure[dir]
 end
+
+p constant_dance('up', TILES)
+p constant_dance('right-down', TILES)
