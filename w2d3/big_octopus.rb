6045413fe7727498df4f6245412b3f91389361d2
Tu Ngo <tungo8810@gmail.com>
2017-04-08 13:00:45 -0700

    Add #dominant

--- w2d3/big_octopus.rb
+++ w2d3/big_octopus.rb
@@ -25,12 +25,38 @@ end
 
 p sluggish(fishes)
 
-def dominant(fishes)
+def merge_sort(arr)
+  return arr if arr.size <= 1
 
+  pivot = arr.size / 2
+  left = merge_sort(arr.take(pivot))
+  right = merge_sort(arr.drop(pivot))
+
+  merge(left, right)
 end
 
-def clever(fishes)
+def merge(left, right)
+  result = []
 
+  until left.empty? || right.empty?
+    if left.first.length < right.first.length
+      result << left.shift
+    else
+      result << right.shift
+    end
+  end
+
+  result += left + right
+end
+
+def dominant(fishes)
+  merge_sort(fishes).last
+end
+
+p dominant(fishes)
+
+def clever(fishes)
+  
 end
 
 def slow_dance(dir, tiles)
