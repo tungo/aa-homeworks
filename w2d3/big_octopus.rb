4e1994d21f1c72ede76c28984edaf6c11fb7e6e2
Tu Ngo <tungo8810@gmail.com>
2017-04-08 12:59:40 -0700

    Add #sluggish

--- w2d3/big_octopus.rb
+++ w2d3/big_octopus.rb
@@ -1,5 +1,29 @@
+fishes = [
+  'fish',
+  'fiiish',
+  'fiiiiish',
+  'fiiiish',
+  'fffish',
+  'ffiiiiisshh',
+  'fsh',
+  'fiiiissshhhhhh'
+]
+# => "fiiiissshhhhhh"
+
 def sluggish(fishes)
+  fishes.each do |fish|
+    longest = true
+    fishes.each do |fs|
+      next if fs == fish
+      longest = false if fish.length < fs.length
+    end
+    return fish if longest
+  end
+
+  nil
 end
+
+p sluggish(fishes)
 
 def dominant(fishes)
 
