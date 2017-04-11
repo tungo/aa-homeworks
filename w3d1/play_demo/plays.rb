4742ab9a5377285f8e5b3ccdb96467197c0e2aa8
Tu Ngo <tungo8810@gmail.com>
2017-04-11 05:06:15 -0700

    Add Playwright #initialize #create #update methods

--- w3d1/play_demo/plays.rb
+++ w3d1/play_demo/plays.rb
@@ -99,4 +99,35 @@ class Playwright
     Playwright.new(result)
   end
 
+  def initialize(options)
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights ('name', 'birth_year')
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?,
+        birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
 end
