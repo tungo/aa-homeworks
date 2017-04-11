bead7fa2dc3971bbcb5c3fae152f2c0cc4fae635
Tu Ngo <tungo8810@gmail.com>
2017-04-11 04:57:33 -0700

    Add Play#find_by_title and Play#find_by_playwright methods

--- w3d1/play_demo/plays.rb
+++ w3d1/play_demo/plays.rb
@@ -19,6 +19,34 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    result = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+
+    Play.new(result)
+  end
+
+  def self.find_by_playwright(name)
+    result = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        plays.*
+      FROM
+        plays
+      JOIN
+        playwrights ON playwrights.id = plays.playwright_id
+      WHERE
+        playwrights.name = ?
+    SQL
+
+    Play.new(result)
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
