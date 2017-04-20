34ffdd5561329fdd628bc7bdc7abca11ae25b3b1
Tu Ngo <tungo8810@gmail.com>
2017-04-20 09:36:55 -0700

    Add UserMailer#welcome_email

--- w4d3/ninety-nine-cat-auth/app/mailers/user_mailer.rb
+++ w4d3/ninety-nine-cat-auth/app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'everybody@appacademy.oi'
+
+  def welcome_email(user)
+    @user = user
+
+    mail(to: @user.email, subject: "Welcome to my site")
+  end
 end
