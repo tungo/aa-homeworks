781a5d1edff7ca95557274f5b6a3922f9a809cf3
Tu Ngo <tungo8810@gmail.com>
2017-04-21 08:53:47 -0700

    Add #is_password? rspec

--- w4d4/aa-music-app/spec/models/user_spec.rb
+++ w4d4/aa-music-app/spec/models/user_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe User, type: :model do
   subject(:user) do
     FactoryGirl.build(:user,
       email: 'tester@appacademy.io',
-      password: 'a perfect password')
+      password: 'a password')
   end
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
+
+  describe '#is_password?' do
+    it "verifies a correct password" do
+      expect(user.is_password?('a password')).to be true
+    end
+
+    it 'verifies a incorrect password' do
+      expect(user.is_password?('not password')).to be false
+    end
+  end
 end
