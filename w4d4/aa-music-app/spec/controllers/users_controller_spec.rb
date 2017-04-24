c46884bcacb227ff36bfcbf0db38e5c3482127a7
Tu Ngo <tungo8810@gmail.com>
2017-04-23 20:29:30 -0700

    Add UsersController rspec

---
+++ w4d4/aa-music-app/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do
+        post :create, user: { username: "", password: "" }
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post :create, user: { username: "user1", password: "min"}
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to bands index on success"
+    end
+  end
+end
