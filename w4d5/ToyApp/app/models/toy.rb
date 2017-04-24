4de6d5e4d729db39a57858b2a79b54eff08d3a4b
Tu Ngo <tungo8810@gmail.com>
2017-04-23 21:32:46 -0700

    Add Toy validation and association

--- w4d5/ToyApp/app/models/toy.rb
+++ w4d5/ToyApp/app/models/toy.rb
@@ -1,2 +1,6 @@
 class Toy < ActiveRecord::Base
+  validates :name, :toyable_id, :toyable_type, presence: true
+  validates :name, uniqueness: { scope: [:toyable_id, :toyable_type] }
+
+  belongs_to :toyable, polymorphic: true
 end
