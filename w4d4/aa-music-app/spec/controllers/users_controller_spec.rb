db3e6d8884aa19d10229bf43c38059a17d5855af
Tu Ngo <tungo8810@gmail.com>
2017-04-23 20:31:05 -0700

    Add Post #create rspec

--- w4d4/aa-music-app/spec/controllers/users_controller_spec.rb
+++ w4d4/aa-music-app/spec/controllers/users_controller_spec.rb
@@ -18,14 +18,18 @@ RSpec.describe UsersController, type: :controller do
       end
 
       it "validates that the password is at least 6 characters long" do
-        post :create, user: { username: "user1", password: "min"}
+        post :create, user: { username: "user1", password: "min" }
         expect(response).to render_template("new")
         expect(flash[:errors]).to be_present
       end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+      it "redirects user to bands index on success" do
+        post :create, user: { email: "user@appa.io", password: "password" }
+        expect(response).to redirect_to(new_session_url)
+      end
     end
   end
+
 end
