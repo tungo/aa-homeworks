78c796c1c549cea929e47d5ce17a775246b59062
Tu Ngo <tungo8810@gmail.com>
2017-05-02 05:23:06 -0700

    Add paradeHelper method

--- w5d2/js-problems/additional.js
+++ w5d2/js-problems/additional.js
@@ -34,3 +34,17 @@ Elephant.prototype.play = function () {
   let trick = this.tricks[Math.floor(Math.random() * (this.tricks.length))];
   console.log(`${this.name} is ${trick}!`);
 };
+
+Elephant.prototype.paradeHelper = function(elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+
+let herd = [ellie, charlie, kate, micah];
+
+herd.forEach(Elephant.prototype.paradeHelper);
