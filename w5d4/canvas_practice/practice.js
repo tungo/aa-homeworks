6a564dff36cd4b60f6a17fb73eca4af5a44c5a85
Tu Ngo <tungo8810@gmail.com>
2017-05-02 03:54:25 -0700

    Draw a cycle

--- w5d4/canvas_practice/practice.js
+++ w5d4/canvas_practice/practice.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", function(){
   canvas.height = 500;
   const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = 'green';
   ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(200, 200, 100, 0, 2 * Math.PI, true);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 5;
+  ctx.stroke();
 
 });
