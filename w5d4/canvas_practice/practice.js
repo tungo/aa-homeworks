d1cd58ae643337b351e75fb3615cf5c32642a825
Tu Ngo <tungo8810@gmail.com>
2017-05-02 03:47:13 -0700

    Set up canvas element

--- w5d4/canvas_practice/practice.js
+++ w5d4/canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  const canvas = document.getElementById('mycanvas') ;
+  canvas.width = 500;
+  canvas.heigth = 500;
+  const ctx = canvas.getContext('2d');
 });
