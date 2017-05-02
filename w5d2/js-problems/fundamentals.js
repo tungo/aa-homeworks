c25ead6a684cbf55cee4a7d21149c6538154cc14
Tu Ngo <tungo8810@gmail.com>
2017-05-02 04:23:52 -0700

    Add isSubstring function

--- w5d2/js-problems/fundamentals.js
+++ w5d2/js-problems/fundamentals.js
@@ -8,3 +8,10 @@ function madLib(verb, adjective, noun) {
 }
 
 // console.log(madLib('make', 'best', 'guac'));
+
+function isSubstring(searchString, subString) {
+  return searchString.includes(subString);
+}
+
+// console.log(isSubstring("time to program", "time"));
+// console.log(isSubstring("Jump for joy", "joys"));
