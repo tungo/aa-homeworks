ec0f929edd341553464f9e02ea1eeda43c7c19d1
Tu Ngo <tungo8810@gmail.com>
2017-05-02 03:49:56 -0700

    Fill rect in canvas

--- w5d4/canvas_practice/practice.js
+++ w5d4/canvas_practice/practice.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
   const canvas = document.getElementById('mycanvas') ;
   canvas.width = 500;
-  canvas.heigth = 500;
+  canvas.height = 500;
   const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, 500, 500);
+
 });
