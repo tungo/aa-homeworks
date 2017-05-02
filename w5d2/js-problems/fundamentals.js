cea69bfeb6b4ff561b8ff8e693f36a72d8e6bfa7
Tu Ngo <tungo8810@gmail.com>
2017-05-02 04:21:06 -0700

    Add madLib function

---
+++ w5d2/js-problems/fundamentals.js
@@ -0,0 +1,10 @@
+
+function madLib(verb, adjective, noun) {
+  let verbUp = verb.toUpperCase();
+  let adjUp = adjective.toUpperCase();
+  let nounUp = noun.toUpperCase();
+
+  return `We shall ${verbUp} the ${adjUp} ${nounUp}`;
+}
+
+// console.log(madLib('make', 'best', 'guac'));
