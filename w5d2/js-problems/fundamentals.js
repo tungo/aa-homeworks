a70153f3c7f044b96990909d5dac4571455c26a0
Tu Ngo <tungo8810@gmail.com>
2017-05-02 04:28:50 -0700

    Add fizzBuzz function

--- w5d2/js-problems/fundamentals.js
+++ w5d2/js-problems/fundamentals.js
@@ -15,3 +15,17 @@ function isSubstring(searchString, subString) {
 
 // console.log(isSubstring("time to program", "time"));
 // console.log(isSubstring("Jump for joy", "joys"));
+
+function fizzBuzz(array) {
+  let result = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if ((array[i] % 3 === 0 || array[i] % 5 === 0) && array[i] % 15 !== 0) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
+
+// console.log(fizzBuzz([1, 3, 5, 9, 10, 15, 30, 31, 35]));
