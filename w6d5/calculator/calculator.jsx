82d893802307ec0124e80b9733582657541af51b
Tu Ngo <tungo8810@gmail.com>
2017-05-05 09:48:51 -0700

    Update Calculator#render and fix bug

--- w6d5/calculator/calculator.jsx
+++ w6d5/calculator/calculator.jsx
@@ -4,13 +4,13 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
 
-    this.states = { result: 0, num1: "", num2: ""};
+    this.state = { result: 0, num1: "", num2: ""};
   }
 
   render() {
     return (
       <div>
-        <h1>Hello world!</h1>
+        <h1>{this.state.result}</h1>
       </div>
     );
   }
