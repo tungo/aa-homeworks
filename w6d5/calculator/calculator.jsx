a73f56d36d04a4205190968a57b560263b63adc8
Tu Ngo <tungo8810@gmail.com>
2017-05-06 09:03:16 -0700

    Add inputs

--- w6d5/calculator/calculator.jsx
+++ w6d5/calculator/calculator.jsx
@@ -5,12 +5,28 @@ class Calculator extends React.Component {
     super(props);
 
     this.state = { result: 0, num1: "", num2: ""};
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+  }
+
+  setNum1(e) {
+    let num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+
+  setNum2(e) {
+    let num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={this.state.num1}></input>
+        <input onChange={this.setNum2} value={this.state.num2}></input>
+        <br/>
       </div>
     );
   }
