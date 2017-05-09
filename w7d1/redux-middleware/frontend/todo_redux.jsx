0d1bae59af4f9cf778cbead43bb85df246483c86
Tu Ngo <tungo8810@gmail.com>
2017-05-09 08:21:15 -0700

    Refactor to applyMiddlewares

--- w7d1/redux-middleware/frontend/todo_redux.jsx
+++ w7d1/redux-middleware/frontend/todo_redux.jsx
@@ -4,22 +4,32 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = (store) => {
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+
+  let result = next(action);
+  console.log(store.getState());
+
+  return result;
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
   let dispatch = store.dispatch;
-  return (action) => {
-    console.log(store.getState());
-    console.log(action);
-    dispatch(action);
-    console.log(store.getState());
-  };
+
+  middlewares.forEach(middleware => (
+    dispatch = middleware(store)(dispatch)
+  ));
+
+  return Object.assign({}, store, { dispatch });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
 
-  store.dispatch = addLoggingToDispatch(store);
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
