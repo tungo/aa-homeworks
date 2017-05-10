658c161d03f14cf951ce8a4b653b94a751a07234
Tu Ngo <tungo8810@gmail.com>
2017-05-10 08:48:09 -0700

    Add initial root

--- w7d2/giphy/components/root.jsx
+++ w7d2/giphy/components/root.jsx
@@ -2,3 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import GiphysSearchContainer from './giphys_search_container';
+
+const Root = (props) => (
+  <h1>Giphy</h1>
+);
+
+export default Root;
--- w7d2/giphy/giphy_search.jsx
+++ w7d2/giphy/giphy_search.jsx
@@ -2,3 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+const store = configureStore();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, root);
+});
+
+// testing
+// import { fetchSearchGiphys } from "./util/api_util";
+// import { receiveSearchGiphys } from "./actions/giphy_actions";
+//
+// window.store = store;
+// store.getState();
+// //should return the initial state object, including the empty giphys array
+//
+// const fetchSuccess = giphys => store.dispatch(receiveSearchGiphys(giphys));
+// fetchSearchGiphys("puppies").then(fetchSuccess);
+//
+// store.getState();
+// // should return the app state populated with puppy giphys
