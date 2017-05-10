9d71eda5ba837a07296caa474bb8f74d8b6b90ff
Tu Ngo <tungo8810@gmail.com>
2017-05-10 08:27:26 -0700

    Add receiveSearchGiphys action creator

--- w7d2/giphy/actions/giphy_actions.js
+++ w7d2/giphy/actions/giphy_actions.js
@@ -1,1 +1,8 @@
 import * as APIUtil from '../util/api_util';
+
+export const RECEIVE_SEARCH_GIPHYS = 'RECEIVE_SEARCH_GIPHYS';
+
+export const receiveSearchGiphys = giphys => ({
+  type: RECEIVE_SEARCH_GIPHYS,
+  giphys
+});
