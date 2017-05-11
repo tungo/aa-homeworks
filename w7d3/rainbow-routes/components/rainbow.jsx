f995759d7f4f068ee3cecdf665fb587d270d811e
Tu Ngo <tungo8810@gmail.com>
2017-05-11 08:31:31 -0700

    Add Links to rainbow.jsx

--- w7d3/rainbow-routes/components/rainbow.jsx
+++ w7d3/rainbow-routes/components/rainbow.jsx
@@ -16,6 +16,10 @@ class Rainbow extends React.Component {
       <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
+        <Link to="/red">Red</Link>
+        <Link to="/green">Green</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/violet">Violet</Link>
 
         <div id="rainbow">
           {/* Your routes should go here */}
