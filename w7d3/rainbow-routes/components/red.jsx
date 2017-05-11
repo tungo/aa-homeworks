631daf4ea9f4f8e76aa28c20c6dbee183e46718c
Tu Ngo <tungo8810@gmail.com>
2017-05-11 08:33:05 -0700

    Add Links to other components

--- w7d3/rainbow-routes/components/red.jsx
+++ w7d3/rainbow-routes/components/red.jsx
@@ -9,7 +9,9 @@ class Red extends React.Component {
       <div>
         <h2 className="red"></h2>
         {/* Links here */}
-
+        <Link to='/red' >Red only</Link>
+        <Link to='/red/orange' >Add Orange</Link>
+        <Link to='/red/yellow' >Add Yellow</Link>
         {/* Routes here */}
         <Route path="/red/orange" component={Orange} />
         <Route path="/red/yellow" component={Yellow} />
